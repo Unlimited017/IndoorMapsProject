da33d2e00c9ebbee7b34daf8e61468615796df80
clavis <clavis.kj@gmail.com>
2018-04-24 21:23:41 +0700

    Update Route.java

--- app/src/main/java/com/example/indoormapsproject/Route.java
+++ app/src/main/java/com/example/indoormapsproject/Route.java
@@ -2,7 +2,6 @@ package com.example.indoormapsproject;
 
 import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,17 +18,24 @@ public class Route {
     }
     public double calculateTotalDistance(){
         int storesSize = this.getStores().size();
+
         Log.i("STORE SIZE IS  "," " + (this.getStores()));
          double ret = (double)(this.getStores().stream().mapToDouble(x -> {
             int storeIndex = this.getStores().indexOf(x);
             double returnValue = 0;
             if(storeIndex < storesSize-1) {
                 returnValue = x.measureDistance(this.getStores().get(storeIndex+1));}
-
+                Log.i("Return Value : " ,Double.toString(returnValue));
             return returnValue;
         }).sum() );
         Log.i("RETURN VALUE IS  "," " + (ret));
         return ret;
+    }
+    public double calculateTotalDuration(){
+        // 1.4 = Average walk time (1.4 m/s)
+        double time = calculateTotalDistance() / 1.4 ; //second
+        Log.i("Time : " ,Double.toString(time));
+        return time;
     }
 
     public String toString() {
